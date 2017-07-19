431003c5e0d0496bc17468fdd745733a8cfa642e
Andreas Fruth <andreas.fruth@gmail.com>
2017-07-19 22:41:25 +0300

    Finished accounts, added authentication example

--- imports/ui/pages/Authentication/Login.jsx
+++ imports/ui/pages/Authentication/Login.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { LoginButtons } from 'meteor/okgrow:accounts-ui-react';
+import Center from '../../components/utils/Center/Center.jsx';
+import './Login.css';
 
 class Login extends React.Component {
     render() {
         return (
-            <LoginButtons visible />
+            <Center>
+                <LoginButtons visible />
+            </Center>
         )
     }
 }
